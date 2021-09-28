9222be9e8c93cc939042563e20e29eba3bc95da5
FilipStaaf <69985953+FilipStaaf@users.noreply.github.com>
2021-09-28 08:34:33 +0200

    Initial commit

---
+++ src/main/java/com/example/rsp_tdd/RspTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsp_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RspTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RspTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rsp_tdd/RspTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsp_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RspTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
